1b12d119d104311405a3e4649c5728a61691e3e8
Miguel Barreto <miguelandres@users.noreply.github.com>
2023-12-06 20:09:14 -0500

    pre-commit run

--- src/main/kotlin/Day03.kt
+++ src/main/kotlin/Day03.kt
@@ -3,30 +3,34 @@ import utils.readAocInput
 import java.util.regex.Pattern
 
 sealed interface EngineSchematicComponent
+
 class PartNumber(val number: Int, var touched: Boolean = false) : EngineSchematicComponent {
   override fun toString(): String = "PartNumber $number${if (touched) " touched" else ""}"
 }
 
 data object EmptySpace : EngineSchematicComponent
+
 class Symbol(val symbol: Char, val nearbyNumbers: MutableSet<PartNumber> = mutableSetOf()) : EngineSchematicComponent
 
 fun main() {
-  val map = readAocInput(3, 1).map { line ->
-    val numberStrings = line.split(Pattern.compile("""\D+""")).filterNot { it.isEmpty() }
-    val numberQueue = numberStrings.flatMap { number ->
-        val partNumber = PartNumber(number.toInt())
-        number.indices.map { partNumber }
-      }.toMutableList()
-    line.map {
-      if (it.isDigit()) {
-        numberQueue.removeFirst()
-      } else if (it == '.') {
-        EmptySpace
-      } else {
-        Symbol(it)
-      }
-    }.toTypedArray()
-  }.toTypedArray().let { Map2D(it) }
+  val map =
+    readAocInput(3, 1).map { line ->
+      val numberStrings = line.split(Pattern.compile("""\D+""")).filterNot { it.isEmpty() }
+      val numberQueue =
+        numberStrings.flatMap { number ->
+          val partNumber = PartNumber(number.toInt())
+          number.indices.map { partNumber }
+        }.toMutableList()
+      line.map {
+        if (it.isDigit()) {
+          numberQueue.removeFirst()
+        } else if (it == '.') {
+          EmptySpace
+        } else {
+          Symbol(it)
+        }
+      }.toTypedArray()
+    }.toTypedArray().let { Map2D(it) }
 
   // Touch all part numbers
   map.forEachWithPosition { pos, value ->
@@ -43,8 +47,9 @@ fun main() {
   val part1 = map.filterIsInstance<PartNumber>().filter { it.touched }.toSet().sumOf { it.number }
   println(part1)
 
-  val part2 = map.filterIsInstance<Symbol>().filter { it.symbol == '*' && it.nearbyNumbers.size == 2 }
-    .map { it.nearbyNumbers.fold(1) { result, next -> result * next.number } }.sum()
+  val part2 =
+    map.filterIsInstance<Symbol>().filter { it.symbol == '*' && it.nearbyNumbers.size == 2 }
+      .map { it.nearbyNumbers.fold(1) { result, next -> result * next.number } }.sum()
 
   println(part2)
 }
